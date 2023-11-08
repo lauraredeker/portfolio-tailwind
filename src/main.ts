b2f8f4c6558d770b979d4db5f124b4ec59307d8b
Laura A. <lauraaredeker@gmail.com>
2023-11-08 20:44:48 +0100

    fix missing import

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import BaseBtn from './components/BaseBtn.vue'
 import App from './App.vue'
 import { routes } from './routes'
 import './index.css'
