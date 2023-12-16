2d2bca51a5fe08dfd15e03f76a17add4829ed6e7
Laura A. <lauraredeker.ux@gmail.com>
2023-12-17 00:34:10 +0100

    update layout

--- src/components/features/TheQuote.vue
+++ src/components/features/TheQuote.vue
@@ -5,14 +5,13 @@
 
   // Composables
   import { useVisibility } from '../../composables/useVisibility'
-
   const [isQuoteVisible, onQuoteVisibility] = useVisibility()
 </script>
 
 <template>
   <div
     v-element-visibility="onQuoteVisibility"
-    class="xl:tw-mt-54 tw-relative tw-z-0 tw-mx-auto -tw-mt-48 tw-flex tw-max-w-full tw-flex-row md:tw-mt-64 md:tw-p-8 2xl:tw-mt-80 2xl:tw-w-3/4 3xl:tw-w-4/6"
+    class="tw-relative tw-z-0 tw-mx-auto -tw-mt-48 tw-flex tw-max-w-full tw-flex-row md:tw-mt-0 md:tw-p-8 xl:tw-mt-80 2xl:tw-mt-80 2xl:tw-w-3/4 3xl:tw-w-4/6"
   >
     <blockquote
       lang="en"
@@ -20,7 +19,7 @@
         'animate__animated animate__fadeIn animate__slow': isQuoteVisible,
         'tw-will-change': isQuoteVisible,
       }"
-      class="tw-border-indigo-400 tw-bg-indigo-300 tw-bg-opacity-40 tw-px-8 tw-pb-32 tw-pt-36 tw-text-xl tw-font-semibold tw-leading-tight tw-text-indigo-900 dark:tw-border-indigo-800 dark:tw-bg-indigo-900  dark:tw-text-indigo-300 sm:tw-text-xl md:tw-mb-10 md:tw-border-[10px] md:tw-p-10 md:tw-px-20 md:tw-text-2xl md:tw-text-indigo-700 md:dark:tw-bg-transparent lg:tw-text-3xl xl:tw-text-5xl"
+      class="tw-border-indigo-400  tw-bg-indigo-300 tw-bg-opacity-30 tw-px-8 tw-pb-32 tw-pt-36 tw-text-xl tw-font-semibold tw-leading-tight tw-text-indigo-900 dark:tw-border-indigo-800 dark:tw-bg-indigo-900 dark:tw-text-indigo-300 sm:tw-text-xl  md:tw-mb-10 md:tw-border-[10px] md:tw-p-10  md:tw-text-2xl md:tw-text-indigo-700 md:dark:tw-bg-transparent lg:tw-text-3xl xl:tw-text-5xl"
     >
       <span
         class="tw-text-m tw-i-ph-quotes-fill tw-mr-2 tw-inline-block md:tw-text-3xl xl:tw-text-5xl"
--- src/components/features/TheTestimonials.vue
+++ src/components/features/TheTestimonials.vue
@@ -1,16 +1,32 @@
 <script lang="ts" setup>
-  import 'vue3-carousel/dist/carousel.css'
+  // Components
   import { Carousel, Slide, Navigation, Pagination } from 'vue3-carousel'
+  import 'vue3-carousel/dist/carousel.css'
   import BaseQuote from '../common/BaseQuote.vue'
+  import { vElementVisibility } from '@vueuse/components'
+
+  // Composables
+  import { useVisibility } from '../../composables/useVisibility'
+  const [isQuoteVisible, onQuoteVisibility] = useVisibility()
 </script>
 
 <template>
   <div
+    v-element-visibility="onQuoteVisibility"
     class="custom-parallax tw-my-20"
   >
-    <div class="tw-py-30 tw-flex tw-h-[80vh] tw-flex-row tw-items-center tw-bg-blue-100 tw-bg-opacity-40 dark:tw-bg-black dark:tw-bg-opacity-50 md:tw-h-screen md:tw-py-52">
+    <div class="tw-py-30 tw-flex tw-h-[70vh] tw-flex-row tw-items-center tw-bg-indigo-200 tw-bg-opacity-60  dark:tw-bg-black dark:tw-bg-opacity-50 md:tw-h-screen md:tw-py-52">
       <Carousel
-        class="focus:tw-outline-none"
+        :class="{
+          'animate__animated animate__fadeIn animate__slow animate__delay-5s': isQuoteVisible,
+        }"
+        class="tw-mx-auto focus:tw-outline-none 2xl:tw-max-w-screen-2xl 3xl:tw-max-w-screen-3xl"
+        :i18n="{
+          'ariaNextSlide': 'Zum nächsten Slide',
+          'ariaPreviousSlide': 'Zur vorherigen Slide',
+          'ariaNavigateToSlide': 'Springe zu Slide {slideNumber}',
+          'itemXofY': 'Slide {currentSlide} von {slidesCount}',
+        }"
         :items-to-show="1"
         :wrap-around="true"
         :autoplay="8000"
@@ -26,12 +42,12 @@
                 <span class="tw-inline-block tw-leading-tight">
                   Lukas Ponikowski<br>
                   Senior UX/UI Designer 
-                  {{ $t('general.at') }} &ThinSpace;
+                  {{ $t('general.at') }}
                   <a
                     href="https://neverdone.app/"
                     target="_blank"
                     rel="noopener noreferrer"
-                    class="tw-inline-block tw-rounded-lg tw-text-indigo-700 tw-underline tw-underline-offset-4 tw-transition-colors hover:tw-text-purple-700 focus-visible:tw-outline-none focus-visible:tw-ring-4 focus-visible:tw-ring-indigo-500 dark:tw-text-purple-200 dark:hover:tw-bg-black"
+                    class="tw-inline-block tw-rounded-lg tw-px-1 tw-text-white tw-underline tw-underline-offset-4 tw-drop-shadow-md tw-transition-colors focus-visible:tw-outline-none focus-visible:tw-ring-4 focus-visible:tw-ring-indigo-500 dark:tw-text-purple-200 dark:hover:tw-text-purple-50"
                   >
                     neverdone
                   </a>
@@ -55,7 +71,7 @@
         </template>
         <template #addons>
           <Navigation class="tw-hidden md:tw-block" />
-          <Pagination class="md:tw-hidden" />
+          <Pagination class="tw-mt-10 md:tw-hidden" />
         </template>
       </Carousel>
     </div>
@@ -65,6 +81,20 @@
 
 
 <style>
+:root { 
+    --vc-pgn-width: 8px;
+    --vc-pgn-height: 8px;
+    --vc-pgn-margin: 8px;
+    --vc-pgn-border-radius: 8px;
+}
+
+.carousel__pagination-button::after {
+  @apply tw-bg-gray-300;
+}
+.carousel__pagination-button:hover::after, .carousel__pagination-button--active::after {
+  @apply tw-bg-purple-200;
+}
+
 .carousel__prev,
 .carousel__next {
   @apply dark:tw-text-purple-200 hover:dark:tw-text-purple-50 tw-rounded-full tw-h-16 tw-w-16 focus:tw-outline-none focus-visible:tw-ring-4 focus-visible:tw-ring-purple-500;
@@ -77,22 +107,19 @@
 }
 
 .custom-parallax {
-  /* Create the parallax scrolling effect */
   background-position: center;
   background-repeat: no-repeat;
-  min-height: 50vh;
-
-  /* Smallest image for phone screens */
-  background-size: 100vw auto;
+  min-height: 70vh;
+  background-size: cover;
+  background-attachment: scroll;
   background-image: image-set(
     url("../img/pictures/banner-moi_mobile.webp") type("image/webp"),
     url("../img/pictures/banner-moi_mobile.jpg") type("image/jpeg")
   );
   
   @screen md {
-    background-attachment: fixed;
     min-height: 100vh;
-    background-size: cover;
+    background-attachment: fixed;
     background-image: image-set(
       url("../img/pictures/banner-moi.webp") type("image/webp"),
       url("../img/pictures/banner-moi.jpg") type("image/jpeg")
